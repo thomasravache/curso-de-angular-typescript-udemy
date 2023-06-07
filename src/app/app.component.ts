8fa0bc2dd1ce638c3d0e0706ed85b22af7417b1a
Thomas Ravache <thomasravache31@gmail.com>
2023-06-07 08:45:39 -0300

    feat: ngDoCheck

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked } from '@angular/core';
+
+/*
+  DoCheck,
+  AfterContentInit,
+  AfterContentChecked,
+  AfterViewInit,
+  AfterViewChecked
+*/
 
 @Component({
   selector: 'app-root',
   // transferir pra cá o router pra ficar mais organizado
   template: `
     <app-title title="Olá mundo"></app-title>
+    {{valor}}
+    <button (click)="adicionar()">Adicionar</button>
     <router-outlet></router-outlet>
   `,
   // tem mais propriedades a serem utilizadas
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, DoCheck, AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked {
   // Propriedade pública
   // public title = "Bem vindo!";
 
+  public valor: number = 1;
+
   constructor() {
 
+  }
+
+  public adicionar(): number {
+    return this.valor += 1;
   }
 
   ngOnInit(): void {
     setTimeout(() => {
       console.log(1);
     }, 5000)
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  } 
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  } 
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  } 
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  } 
+  ngAfterViewChecked() {
+    console.log('ngAfterViewCHecked');
   }
 }
