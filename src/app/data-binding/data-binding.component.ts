75b861246813647606126b18f723ab195b5bc07e
Thomas Ravache <thomasravache31@gmail.com>
2023-06-12 08:44:49 -0300

    feat: Property Binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -9,4 +9,11 @@ export class DataBindingComponent {
   public nome: string = "Thomas";
   public idade: number = 24;
   public maisUm: number = 1;
+
+  public checkedDisabled: boolean = false;
+  public imgSrc: string = "https://th.bing.com/th/id/OIP.SUAt8oYIRxCnXNUOVMmmvAHaG2?w=197&h=183&c=7&r=0&o=5&pid=1.7";
+  public imgTitle: string = "Imagem teste"
+
+  constructor () {}
+
 }
