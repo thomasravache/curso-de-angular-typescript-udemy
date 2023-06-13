7088dd43e5d9f025be592bf2f3090fbc7250447d
Thomas Ravache <thomasravache31@gmail.com>
2023-06-13 06:25:02 -0300

    feat: Event Binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -14,6 +14,17 @@ export class DataBindingComponent {
   public imgSrc: string = "https://th.bing.com/th/id/OIP.SUAt8oYIRxCnXNUOVMmmvAHaG2?w=197&h=183&c=7&r=0&o=5&pid=1.7";
   public imgTitle: string = "Imagem teste"
 
+  public position: { x: number, y: number } = { x: 0, y: 0 };
+
   constructor () {}
 
+  public alertaInfo(valor: MouseEvent): void {
+    console.log(valor);
+  }
+
+  public mouseMoveTeste(valor: MouseEvent) {
+    // console.log(valor);
+    this.position.x = valor.offsetX;
+    this.position.y = valor.offsetY;
+  }
 }
