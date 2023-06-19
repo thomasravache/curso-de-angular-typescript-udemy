3e3cd3decee6340aab8327ef9ce7bc15c5aa726f
Thomas Ravache <thomasravache31@gmail.com>
2023-06-19 08:06:46 -0300

    feat: ng-content

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,11 @@ import { Component, OnInit, DoCheck, AfterContentInit, AfterContentChecked, Afte
   template: `
     <!-- <app-data-binding></app-data-binding> -->
     <!-- <app-diretivas-estruturais></app-diretivas-estruturais> -->
-    <app-diretivas-atributos></app-diretivas-atributos>
+
+    <app-diretivas-atributos>
+      <h1>Aula de Diretivas de Atributo</h1>
+      <hr>
+    </app-diretivas-atributos>
     <router-outlet></router-outlet>
   `,
   // tem mais propriedades a serem utilizadas
