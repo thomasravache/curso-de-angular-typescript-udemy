2eadf26d91460d6dd95ad0fd8b51855493f6f6e0
Thomas Ravache <thomasravache31@gmail.com>
2023-06-26 08:36:20 -0300

    feat: Hands-On 2

---
+++ projects/app-todo/src/app/modules/home/home.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './pages/home/home.component';
+import { TodoButtonDeleteAllComponent } from './components/todo-button-delete-all/todo-button-delete-all.component';
+import { TodoListComponent } from './components/todo-list/todo-list.component';
+import { HeaderComponent } from './components/header/header.component';
+import { TodoInputAddItensComponent } from './components/todo-input-add-itens/todo-input-add-itens.component';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent,
+    TodoButtonDeleteAllComponent,
+    TodoListComponent,
+    HeaderComponent,
+    TodoInputAddItensComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class HomeModule { }
