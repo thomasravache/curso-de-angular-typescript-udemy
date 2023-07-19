110bb1d47d29cee39a0cb1826de121a78aa24404
Thomas Ravache <thomasravache31@gmail.com>
2023-07-18 21:20:36 -0300

    feat: 65. ngForm + Selected

--- aprendizados-01/src/app/forms/forms.component.ts
+++ aprendizados-01/src/app/forms/forms.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent {
-
+  public listComidas: Array<{ comida: string }> = [
+    { comida: 'X-Salada' },
+    { comida: 'X-Bacon' },
+    { comida: 'Coxinha' }
+  ];
 }
