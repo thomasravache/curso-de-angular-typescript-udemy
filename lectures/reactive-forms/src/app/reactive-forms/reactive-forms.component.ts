d793cebdc2709eb28e7c9fec6ee8dc0232b57d75
Thomas Ravache <thomasravache31@gmail.com>
2023-08-08 22:38:34 -0300

    feat: 72. Mais sobre Validators

--- lectures/reactive-forms/src/app/reactive-forms/reactive-forms.component.ts
+++ lectures/reactive-forms/src/app/reactive-forms/reactive-forms.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, ValidationErrors, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -9,16 +9,25 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class ReactiveFormsComponent {
   public cadastroForm: FormGroup = this.formBuilder.group({
     firstName: ['', Validators.required],
-    lastName: [''],
+    lastName: ['', [Validators.required, Validators.minLength(5)]],
+    email: ['', [Validators.required, Validators.email]],
   });
 
   constructor(
     private formBuilder: FormBuilder,
   ) { }
 
+  public get teste(): any {
+    return this.cadastroForm.get('lastName')?.errors
+  }
+
+  public getErrors(field: string): ValidationErrors | any {
+    return (this.cadastroForm.get(field) as AbstractControl | any)?.errors;
+  }
+
   public submitForm(): void {
     if (this.cadastroForm.invalid) return;
-    
+
     console.log(this.cadastroForm);
   }
 }
