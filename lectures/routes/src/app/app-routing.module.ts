9b402c3a6a2e819f1732ea86d304dd297dd3d74a
Thomas Ravache <thomasravache31@gmail.com>
2023-08-09 21:03:48 -0300

    feat: configurar app.component

--- lectures/routes/src/app/app-routing.module.ts
+++ lectures/routes/src/app/app-routing.module.ts
@@ -2,8 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: '', component: PageHomeComponent, pathMatch: 'full' }, // patchMatch sempre full quando a rota estiver vazia, pra evitar renderização a mais
-
+  
 ];
 
 @NgModule({
