e6ab42c5ddcebd33a278c57164701b73bf778cb3
Thomas Ravache <thomasravache31@gmail.com>
2023-08-09 21:00:39 -0300

    feat: 73. Entendendo o Arquivo de Rotas

--- lectures/routes/src/app/app-routing.module.ts
+++ lectures/routes/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: PageHomeComponent, pathMatch: 'full' }, // patchMatch sempre full quando a rota estiver vazia, pra evitar renderização a mais
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
