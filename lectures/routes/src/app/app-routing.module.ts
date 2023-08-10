1c6fdf54d950ab61acadc5f2917a93ebb44d36c9
Thomas Ravache <thomasravache31@gmail.com>
2023-08-09 21:14:24 -0300

    feat: 75. Rota Coringa + redirectTo

--- lectures/routes/src/app/app-routing.module.ts
+++ lectures/routes/src/app/app-routing.module.ts
@@ -9,7 +9,8 @@ import { PageErrorComponent } from './pages/page-error/page-error.component';
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'sobre', component: SobreComponent },
-  { path: '404', component: PageErrorComponent }
+  { path: '404', component: PageErrorComponent },
+  { path: '**', redirectTo: '404' } // sempre que digitar uma rota errada ele cai na rota coringa que redireciona pra rota 404 que é a rota de erro
 ];
 
 @NgModule({
