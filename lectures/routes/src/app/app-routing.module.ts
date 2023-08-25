1d9a556761489db22c1bb9b5be42f4ef552a2f0a
Thomas Ravache <thomasravache31@gmail.com>
2023-08-25 08:46:41 -0300

    feat: 78. Parametros nas rotas

--- lectures/routes/src/app/app-routing.module.ts
+++ lectures/routes/src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { PageErrorComponent } from './pages/page-error/page-error.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
-  { path: 'sobre', component: SobreComponent },
+  { path: 'sobre/:id/:username', component: SobreComponent },
   { path: '404', component: PageErrorComponent },
   { path: '**', redirectTo: '404' } // sempre que digitar uma rota errada ele cai na rota coringa que redireciona pra rota 404 que é a rota de erro
 ];
--- lectures/routes/src/app/pages/sobre/sobre.component.ts
+++ lectures/routes/src/app/pages/sobre/sobre.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-sobre',
   templateUrl: './sobre.component.html',
   styleUrls: ['./sobre.component.scss']
 })
-export class SobreComponent {
+export class SobreComponent implements OnInit {
+  constructor(
+    private activedRoute: ActivatedRoute
+  ) {}
 
+    ngOnInit(): void {
+      this.activedRoute.params.subscribe(
+        (res: any) => console.log(res.id, res.username, res)
+      );
+
+      this.activedRoute.queryParams.subscribe(
+        (res) => console.log('queryparams', res)
+      );
+    }
 }
